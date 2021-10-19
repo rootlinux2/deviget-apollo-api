38bebeec24ce420c158a6b5ba01f1e08c6317de0
Yoandry Collazo <ycbarrosocu@gmail.com>
2021-10-19 13:15:10 -0400

    feat: add initial sever configurations

---
+++ index.js
@@ -0,0 +1,56 @@
+import { ApolloServer } from "apollo-server";
+import models from "./models";
+import resolvers from "./resolvers";
+import schema from "./schema";
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+// const typeDefs = gql`
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+//   # This "Book" type defines the queryable fields for every book in our data source.
+//   type Book {
+//     title: String
+//     author: String
+//   }
+
+//   # The "Query" type is special: it lists all of the available queries that
+//   # clients can execute, along with the return type for each. In this
+//   # case, the "books" query returns an array of zero or more Books (defined above).
+//   type Query {
+//     books: [Book]
+//   }
+// `;
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: async ({ req, connection }) => {
+    if (connection) {
+      return {
+        models,
+      };
+    }
+
+    if (req) {
+      // const me = await getMe(req);
+
+      return {
+        models,
+        // me,
+        // // secret: process.env.SECRET,
+        // loaders: {
+        //   user: new DataLoader((keys) => loaders.user.batchUsers(keys, models)),
+        // },
+      };
+    }
+  },
+});
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
